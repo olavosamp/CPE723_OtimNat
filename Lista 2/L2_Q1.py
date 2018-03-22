18042f97510cd5744e2777e328e117aeb7c14d43
olavosamp <olavosamp@poli.ufrj.br>
2018-03-22 00:04:12 -0300

    Lista2 Q1c

--- Lista 2/L2_Q1.py
+++ Lista 2/L2_Q1.py
@@ -19,6 +19,32 @@ print(X3)
 
 # Letra B
 
-print("\nProb 1:", random.random())
-print("\nProb 2:", random.random())
-print("\nProb 3:", random.random())
+def get_random_event(events):
+    random_number = np.random.random()
+    lower_bound = 0
+    for eventProb in events:
+        if lower_bound <= random_number <= lower_bound + eventProb:
+            return events.index(eventProb)
+        else:
+            lower_bound += eventProb
+
+# print("\nProb 1:", random.random())
+# print("\nProb 2:", random.random())
+# print("\nProb 3:", random.random())
+
+# Letra C
+
+N = 100
+t = 3
+
+X = np.empty((N, t+1), dtype=int)
+X[:,0] = np.random.randint(0, np.shape(M)[0],size=N)
+# print(X[:,0].shape)
+print(X.shape)
+
+for i in range(N):
+    for j in range(t):
+        # print(i,j)
+        # print((X[i,j]))
+        X[i,j+1] = get_random_event(M[X[i,j]])
+print(X)
