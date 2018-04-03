ab1092b2cf15f4cb25869f81639965d29e1d3ad7
Olavo Sampaio <olavosamp@poli.ufrj.br>
2018-04-03 12:04:40 -0300

    L3_Q3cd

--- Lista 3/L3_Q1.py
+++ Lista 3/L3_Q1.py
@@ -14,30 +14,36 @@ np.set_printoptions(precision=3, suppress=True)
 
 ## Letra A
 
-# X = np.ndarray([0, 4, 6, 9], dtype=int)
-X = [0, 4, 6, 9]
+# X = np.array([0, 4, 6, 9], dtype=int)
+X = np.array([0, 4, 6, 9])
 t = np.arange(-1.0, 10.0, 0.1)
 D = np.empty(len(t))
 i = 0
-for partition in t:
-    X1 = [elem for elem in X if elem <= partition]
-    X2 = [elem for elem in X if elem > partition]
-
-    y1 = utils.center_mass(X1)
-    y2 = utils.center_mass(X2)
-
-    dist1 = np.sum(utils.euclidean_dist(X1, y1))
-    dist2 = np.sum(utils.euclidean_dist(X2, y2))
-
-    D[i] = (dist1+dist2)/(len(X1)+len(X2))
-
-    # print("\ni: {}, t : {:.1f}\nD: {:.3f}".format(i, partition, D[i]))
-    i += 1
-
-plt.plot(t, D)
-# plt.set_title("Gráfico de D(t)")
-plt.ylabel("D(t)")
-plt.xlabel("t")
-plt.show()
+# for partition in t:
+#     X1 = [elem for elem in X if elem <= partition]
+#     X2 = [elem for elem in X if elem > partition]
+#
+#     y1 = utils.center_mass(X1)
+#     y2 = utils.center_mass(X2)
+#
+#     dist1 = np.sum(utils.rms_error(X1, y1))
+#     dist2 = np.sum(utils.rms_error(X2, y2))
+#
+#     D[i] = (dist1+dist2)/(len(X1)+len(X2))
+#
+#     # print("\ni: {}, t : {:.1f}\nD: {:.3f}".format(i, partition, D[i]))
+#     i += 1
+#
+# plt.plot(t, D)
+# # plt.set_title("Gráfico de D(t)")
+# plt.ylabel("D(t)")
+# plt.xlabel("t")
+# plt.show()
 
 ## Letra B
+
+y = np.array([3.0, 3.4])
+T = 1.0
+P_yx = utils.prob_yx(X, y, T)
+
+print(P_yx)
--- Lista 3/utils.py
+++ Lista 3/utils.py
@@ -1,9 +1,68 @@
 import numpy as np
 
-def euclidean_dist(x, y):
+def rms_error(x, y):
     dist = x - y
     dist = np.power(dist, 2)
     return dist
 
 def center_mass(x):
     return np.sum(x)/len(x)
+
+def prob_yx(x, y, T):
+    '''
+    Compute conditional probabilty matrix P_y|x
+    Arguments:
+        x: N by M numpy array. N is input dimension. M is number of input elements
+        y: N by C numpy array. C is number of centroids.
+        T: Boltzmann-Gibbs distribution temperature.
+    Returns:
+        P: C by M conditional probabilty matrix P_y|x
+    '''
+    # Shenanigans to guarantee correct numpy dimensions
+    if x.ndim == 1:
+        M = len(x)
+        x.shape = (1, M)
+    else:
+        M = np.shape(x)[1]
+
+    if y.ndim == 1:
+        C = len(y)
+        y.shape = (1, C)
+    else:
+        C = np.shape(y)[1]
+
+    # Compute P_y|x
+    P = np.zeros((C, M))
+    for l in range(C):
+        for k in range(M):
+            P[l, k] = np.exp(-rms_error(x[:,k], y[:,l])/T)  # P_(y_l)|(x_k)
+        P[l, :] = P[l, :]/np.sum(P[l, :])                   # Normalization factor u_x
+
+    return P
+
+def cluster_cost(P, x, y):
+    '''
+    Assuming equiprobable inputs
+    '''
+
+    # Shenanigans to guarantee correct numpy dimensions
+    if x.ndim == 1:
+        M = len(x)
+        x.shape = (1, M)
+    else:
+        M = np.shape(x)[1]
+
+    if y.ndim == 1:
+        C = len(y)
+        y.shape = (1, C)
+    else:
+        C = np.shape(y)[1]
+
+    # Compute cost
+    prob_x = 1/M
+    for k in range(M):
+        for l in range(C):
+            cost += P[l, k]*rms_error(x[:, k], y[:, l])
+
+    cost /= prob_x
+    return cost
