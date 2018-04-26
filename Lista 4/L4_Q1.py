c9b2525c02f1b1e843d94c3fca6e55cd64cd2339
Olavo Sampaio <olavosamp@poli.ufrj.br>
2018-04-26 19:45:21 -0300

    Working on L4_Q1

--- Lista 4/L4_Q1.py
+++ Lista 4/L4_Q1.py
@@ -1,11 +1,20 @@
 import numpy                as np
 # import matplotlib.pyplot    as plt
 
+def flipBit(bit):
+    if bit == 0:
+        return 1
+    if bit == 1:
+        return 0
+
 class mazaState:
     """
         docstring for mazaState.
     """
-    def __init__(self, binVal=0):
+    def __init__(self, binVal=0, randomize=False):
+        self.bitLen = 16
+        if randomize:
+            binVal = self.randomState()
         self.setBin(binVal)
 
     def bin2int(self, binVal):
@@ -20,28 +29,28 @@ class mazaState:
             return sign*(intPart + frac)
 
     def setBin(self, binVal):
-        if len(binVal) == 16:
+        if len(binVal) == self.bitLen:
             self.binVal = binVal
             self.intVal = self.bin2int(self.binVal)
         else:
-            raise ValueError("binVal string has wrong number of bits")
-        return self.binVal
+            raise ValueError("binVal string has wrong number of bits:\n\tFound    {}\n\tExpected {}".format(len(binVal), self.bitLen))
 
+        return self.binVal
 
     def aptitude(self):
         x = self.intVal
         apt = x**2 - 0.3*np.cos(10*np.pi*x)
         return apt
 
-# for i in range(-2, 3):
-#     print("{:2d} | {}".format((i), np.binary_repr(i, width=4)))
-#
-# print("")
-# for i in range(-2, 3):
-#     # print("{:2d} | {}".format((i), int(np.binary_repr(i, width=4)), 2))
-#     print("{:2d} | {}".format((i), int(bin(i), base=0)))
+    def randomState(self):
+        return ''.join(np.random.randint(0, 2, size=16).astype('str'))
 
-state1 = mazaState('0001111111111100')
-# state1.setBin('0101111111111111')
+# state1 = mazaState('0001111111111100')
+# randomString = ''.join(np.random.randint(0, 2, size=16).astype('str'))
+
+state1 = mazaState('0001111111111100', randomize=True)
 print("\nState X: ", state1.intVal)
 print("J(X):      ", state1.aptitude())
+
+# print(randomString)
+# print(len(randomString))
