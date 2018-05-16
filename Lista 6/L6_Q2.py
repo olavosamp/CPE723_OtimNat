41e0d5ae2de417e491ccf5ed5e5d10acf7130375
Olavo Sampaio <olavosamp@poli.ufrj.br>
2018-05-16 17:42:15 -0300

    Implemented memetic GA

--- Lista 6/L6_Q2.py
+++ Lista 6/L6_Q2.py
@@ -1,21 +1,56 @@
-import numpy    as np
-import pandas   as pd
+
+import numpy                as np
+import pandas               as pd
+import matplotlib.pyplot    as plt
 import copy
+# from tqdm                   import tqdm
+
 
 from mazaPop    import (MazaPop,
-                        parent_selection_roulette)
+                        parent_selection_roulette,
+                        crossover_one_point,
+                        mutation,
+                        generation)
 
-popSize = 20
-bitLen  = 10
-numParents = 10
-
+popSize = 100
+bitLen  = 25
+# numParents = 10
+mutateProb = 1/bitLen
+crossProb = 0.7
+numGenerations = 100
 
 'x = [x1, x2, ... xn], vector size: 1 by L'
 
 mazaPop = MazaPop(size=popSize, bitLen=bitLen)
 
-# print(mazaPop.pop.axes)
-parentsIndex = parent_selection_roulette(mazaPop.pop, numParents)
-print(mazaPop.pop.sort_values("Aptitude", ascending=False))
-print("")
-print(mazaPop.pop.loc[parentsIndex, :])
+popGen = []
+for i in range(numGenerations):
+    mazaPop.pop = generation(mazaPop.pop, crossProb=crossProb, mutateProb=mutateProb).copy(deep=True)
+    popGen.append(mazaPop.pop)
+
+# Evaluation and plots
+bestFit = []
+popMean = []
+bestFitMeme= []
+popMeanMeme= []
+for i in range(len(popGen)):
+    bestFit.append(popGen[i]["Aptitude"].max())
+    popMean.append(popGen[i]["Aptitude"].mean())
+    bestFitMeme.append(popGen[i]["Memetic"].max())
+    popMeanMeme.append(popGen[i]["Memetic"].mean())
+
+print("\nBest Fitness: {:.3f}".format(bestFit[-1]))
+print("Mean Fitness: {:.3f}".format(popMean[-1]))
+
+plt.plot(bestFit, 'r', label="Best Fitness")
+plt.plot(popMean, 'b', label="Mean Fitness")
+plt.plot(bestFitMeme, 'g', label="Best Memetic Fitness")
+plt.plot(popMeanMeme, 'k', label="Mean Memetic Fitness")
+plt.legend()
+
+# plt.ylim(ymin=-1.0)
+
+plt.xlabel("Generation")
+plt.ylabel("Fitness")
+
+plt.show()
--- Lista 6/mazaPop.py
+++ Lista 6/mazaPop.py
@@ -1,20 +1,111 @@
 import numpy    as np
 import pandas   as pd
-import copy
+from copy       import copy
 
+def bit_flip(bit):
+    if bit == 0:
+        return 1
+    if bit == 1:
+        return 0
+    else:
+        raise ValueError("Argument must be a integer value of 0 or 1")
 
-def compute_aptitude(states):
-    aptitude = np.sum(states, axis=1)
+def compute_aptitudes(state):
+    aptitude = np.sum(state, axis=1)
     return aptitude
 
+def greedy_local_search(state):
+    bitLen = len(state)
+    apt = np.sum(state)
+
+    position = 0
+    newApt = 0
+    while (newApt < apt) and (position < bitLen):
+        newState = copy(state)
+        newState[position] = bit_flip(newState[position])
+
+        newApt = np.sum(newState)
+        position += 1
+
+    return newState, newApt
+
+def compute_memetic_aptitudes(pop):
+    popSize = pop.shape[0]
+    bitLen  = len(pop.loc[0, "State"])
+    # Local search
+    newStates           = np.zeros((popSize, bitLen))
+    memeticAptitudes    = np.zeros(popSize)
+    for i in range(popSize):
+        newStates[i,:], memeticAptitudes[i] = greedy_local_search(pop.loc[i, "State"])
+
+    # tempList = np.array(tempList)
+    # print(tempList[:, 0][1])
+    # input()
+
+    return newStates, memeticAptitudes
+
+'Parent Selection'
 def parent_selection_roulette(population, numParents):
+    '''
+    Input: population DataFrame
+    '''
     popSize = population.shape[0]
 
     probabilities = population["Aptitude"]/population["Aptitude"].sum()
 
-    parentsIndex = np.random.choice(np.arange(popSize), size=numParents, p=probabilities, replace=False)
+    parentsIndex = np.random.choice(range(popSize), size=numParents, p=probabilities, replace=True)
 
     return parentsIndex
+
+'One-Point Crossover'
+def crossover_one_point(parentA, parentB, prob=0.9):
+    '''
+    Input:  A pair of 1-row parent DataFrames
+    Output: A pair of 1-row children DataFrames
+    '''
+    randomNum = np.random.rand()
+
+    if randomNum < prob:
+        childA, childB = recombineAB(parentA["State"], parentB["State"])
+
+        # childA = parentA.copy()
+        # childA["State"] = stateA
+        #
+        # childB = parentB.copy()
+        # childB["State"] = stateB
+    else:
+        childA = parentA.loc[:, "State"]
+        childB = parentB.loc[:, "State"]
+    return childA, childB
+
+'Recombination routine'
+def recombineAB(parentA, parentB, percentA=0.5):
+    '''
+    Input:  A pair of parent state arrays
+    Output: A pair of children state arrays
+    '''
+    if len(parentA) != len(parentB):
+        raise ValueError("Parent lengths must be equal")
+
+    childA = np.zeros(len(parentA), dtype=int)
+    childB = np.zeros(len(parentA), dtype=int)
+    cut = int(round(len(parentA)*percentA))
+
+    childB[cut:] = parentB[cut:].copy()
+    childB[:cut] = parentA[:cut].copy()
+
+    childA[cut:] = parentA[cut:].copy()
+    childA[:cut] = parentB[:cut].copy()
+
+    return childA, childB
+
+def mutation(specimen, prob=0.05):
+    randomNum = np.random.rand()
+    if randomNum < prob:
+        mutatePos = np.random.randint(len(specimen))
+        specimen[mutatePos] = bit_flip(specimen[mutatePos])
+
+    return specimen
 
 class MazaPop:
     '''
@@ -27,11 +118,52 @@ class MazaPop:
         self.bitLen = bitLen
 
         # Initialize binary strings of length bitLen
-        states = []
+        state = []
         for i in range(size):
-            states.append(np.random.randint(0, high=2, size=self.bitLen))
+            state.append(np.random.randint(0, high=2, size=self.bitLen))
 
         # Compute One-max fitness per specimen
-        aptitude = compute_aptitude(states)
+        aptitude = compute_aptitudes(state)
 
-        self.pop = pd.DataFrame(data={"States":states, "Aptitude":aptitude})
+        self.pop = pd.DataFrame(data={"State":state, "Aptitude":aptitude})
+
+    def update_aptitude(self):
+        # Recalculate One-max fitness per specimen
+        aptitude = compute_aptitudes(state)
+        self.pop["Aptitude"] = aptitude
+
+        return aptitude
+
+def generation(pop, crossProb=0.9, mutateProb=0.05):
+    numParents = pop.shape[0]
+
+    zeroPad = np.zeros(pop.shape[0])
+    newPop = pd.DataFrame(data={"State":zeroPad, "Aptitude":zeroPad})
+
+    ## Parent selection
+    parentsIndex = parent_selection_roulette(pop, numParents)
+    parents = pop.loc[parentsIndex, :].reset_index(drop=True)
+
+    ## Recombination
+    # Each parent will create one child
+    childrenStates = []
+    for i in range(0, numParents, 2):
+        childA, childB = crossover_one_point(parents.loc[i, :], parents.loc[i+1, :], prob=1.0)
+        childrenStates.append(childA)
+        childrenStates.append(childB)
+
+    newPop["State"] = childrenStates
+
+    # Update aptitudes
+    newPop["Aptitude"] = list(map(lambda x: np.sum(x), newPop["State"]))
+
+    ## Mutation
+    newPop["State"] = list(map(mutation, newPop["State"]))
+    newPop["Aptitude"] = list(map(lambda x: np.sum(x), newPop["State"]))
+
+    ## Update Memetic Aptitudes
+    newStates, memeticAptitudes = compute_memetic_aptitudes(newPop)
+    newPop["State"] = newStates
+    newPop["Memetic"] = memeticAptitudes
+
+    return newPop
